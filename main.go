98947ef0aadacaae7007722a087dff31b2974386
Monu Carpenter <monuc1315@gmail.com>
2026-03-27 17:14:41 +0530

    fix: swagger docs error

--- main.go
+++ main.go
@@ -27,6 +27,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		e.Use(middleware.Logger())
 		e.Use(middleware.Recover())
 		e.GET("/swagger/*", echoSwagger.WrapHandler)
+		e.GET("/", echoSwagger.WrapHandler)
 		routes.RegisterRoutes(e)
 		echoHandler = e.Server.Handler
 	})
